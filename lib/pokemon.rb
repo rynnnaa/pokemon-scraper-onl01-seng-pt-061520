a960c73d2743a310daa4eb7f58262bf39ac630f0
Ryna Truong <rynatruong@outlook.com>
2020-08-21 22:55:31 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,7 +2,7 @@ class Pokemon
   
   attr_accessor :id, :name, :type, :db
 
-  def initialize(keywords)
+  def initialize(id:, name:, type:, db:)
     @id = id
     @name = name
     @type = type
@@ -18,7 +18,7 @@ class Pokemon
   end
   
    def self.find(id, db)
-     sql = SELECT * FROM pokemon WHERE id=?"
+     sql = "SELECT * FROM pokemon WHERE id=?"
      db.execute(sql, id).map do |row|
        Pokemon.new(id: row[0], name: row[1], type: row[2], db: db)
     end
