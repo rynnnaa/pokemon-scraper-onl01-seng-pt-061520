163218aedc3cd24f6002488c5a1c46ede7aae5f6
Ryna Truong <rynatruong@outlook.com>
2020-08-21 22:27:22 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,20 @@
-class Pokemon
+class Pokemon::
+  
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(keywords)
+    keywords.each {|key, value| self.send(("#{key}="), value)}
+  end
+    # @id = id
+    # @name = name
+    # @type = type
+    # @db = db
+  
+  def self.save(name, type, db)
+    sql = <<-SQL
+    INSERT INTO pokemons(name, type, db) VALUES(?,?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.tye, self.db)
+    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+  end
 end
