6a7ffe943a3649e65d856277330298fc10a159e0
Ryna Truong <rynatruong@outlook.com>
2020-08-21 22:56:54 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -21,6 +21,6 @@ class Pokemon
      sql = "SELECT * FROM pokemon WHERE id=?"
      db.execute(sql, id).map do |row|
        Pokemon.new(id: row[0], name: row[1], type: row[2], db: db)
-     end
+     end.first
    end
 end
